9f1dfe66e20744b4859be841d7a3bc6b10227420
Gromf1 <levchukgeorgy1@gmail.com>
2026-05-15 14:46:29 +0500

    Обновил readme, удалил ненужный класс XmlUtil, в класс Database добавил подготовку statement (один раз и дальнейшее его переиспользование) и Logger.

--- src/main/java/com/example/xmlserver/db/Database.java
+++ src/main/java/com/example/xmlserver/db/Database.java
@@ -1,21 +1,27 @@
 package com.example.xmlserver.db;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Database {
 
+    private static final Logger LOGGER = Logger.getLogger(Database.class.getName());
     private static final String URL = "jdbc:sqlite:messages.db";
     private static final Object DB_LOCK = new Object();
+
+    private static PreparedStatement preparedStatement;
 
     static {
         init();
     }
 
     private static void init() {
-        try (Connection conn = DriverManager.getConnection(URL);
-             Statement stmt = conn.createStatement()) {
+        try {
+            Connection conn = DriverManager.getConnection(URL);
 
-            stmt.execute("""
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute("""
                 CREATE TABLE IF NOT EXISTS messages (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     user TEXT,
@@ -24,29 +30,26 @@ public class Database {
                     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
                 )
             """);
+            }
 
+            preparedStatement = conn.prepareStatement(
+                    "INSERT INTO messages(user, text, code) VALUES (?, ?, ?)"
+            );
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Failed to initialize database", e);
         }
     }
 
     public static void save(String user, String text, int code) {
-
         synchronized (DB_LOCK) {
-
-            String sql = "INSERT INTO messages(user, text, code) VALUES (?, ?, ?)";
-
-            try (Connection conn = DriverManager.getConnection(URL);
-                 PreparedStatement ps = conn.prepareStatement(sql)) {
-
-                ps.setString(1, user);
-                ps.setString(2, text);
-                ps.setInt(3, code);
-
-                ps.executeUpdate();
+            try {
+                preparedStatement.setString(1,user);
+                preparedStatement.setString(2,text);
+                preparedStatement.setInt(3,code);
+                preparedStatement.executeUpdate();
 
             } catch (Exception e) {
-                e.printStackTrace();
+                LOGGER.log(Level.SEVERE, "Failed to save message", e);
             }
         }
     }
--- src/main/java/com/example/xmlserver/util/XmlUtil.java
+++
@@ -1,4 +0,0 @@
-package com.example.xmlserver.util;
-
-public class XmlUtil {
-}
